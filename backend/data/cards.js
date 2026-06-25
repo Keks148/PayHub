311c4280fce20098998d2f50f242d461103809a1
Keks148 <hrabevnyk@gmail.com>
2026-06-25 19:30:38 +0300

    Add files via upload

---
+++ backend/data/cards.js
@@ -0,0 +1,20 @@
+const cards = [
+  {
+    id: "card_1",
+    trader_id: "trader_1",
+    bank: "Monobank",
+    card_number: "5375410000001234",
+    card_holder: "PAYHUB TEST CARD",
+    min_amount: 1000,
+    max_amount: 6000,
+    daily_limit: 500000,
+    max_payments_per_day: 5,
+    payments_today: 0,
+    turnover_today: 0,
+    active: true,
+    reserved: false,
+    current_order_id: null
+  }
+];
+
+module.exports = cards;
---
+++ backend/data/traders.js
@@ -0,0 +1,12 @@
+const traders = [
+  {
+    id: "trader_1",
+    name: "Trader_1",
+    status: "online",
+    percent: 4.5,
+    available_usdt: 374.3,
+    reserved_usdt: 0
+  }
+];
+
+module.exports = traders;
